b1e1485fb4d12c0d512a86652bbc9ea1965ff112
Rod Michael Coronel <rodmaykel@gmail.com>
2014-04-11 00:54:41 +0800

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Twitstagram::Application.routes.draw do
 
   resources :subjects
 
+  match '/', to: 'static_pages#home', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
