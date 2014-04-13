931e447c0ba622284ede01d05a88d9bf5729dc62
Rod Michael Coronel <rodmaykel@gmail.com>
2014-04-14 01:33:12 +0800

    Use layout in rendering title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "About page" do
 
-    it "should have the content 'About Twitstagram'" do
+    it "should have the content 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Twitstagram')
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
     end
   end
 
@@ -15,6 +20,11 @@ describe "StaticPages" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact Us')
     end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact")
+    end
   end
 
   describe "TnC page" do
@@ -22,6 +32,12 @@ describe "StaticPages" do
       visit '/static_pages/tnc'
       expect(page).to have_content('Terms and Conditions')
     end
+
+    it "should have the title 'Terms and Conditions'" do
+      visit '/static_pages/tnc'
+      expect(page).to have_title("Terms and Conditions")
+    end
+
   end
 
 end
